4f2895c9614f92d62a2d595612530b754ae7c6d3
Aidan <111407481+amusasrd@users.noreply.github.com>
2022-11-18 21:11:36 +1100

    Add files via upload

---
+++ CheckSplitter/CheckSplitter/CheckSplitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckSplitterApp.swift
+//  CheckSplitter
+//
+//  Created by A Moses on 12/11/22.
+//
+
+import SwiftUI
+
+@main
+struct CheckSplitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
